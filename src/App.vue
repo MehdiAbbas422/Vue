4e045a1c3dd01e0082432a0b2fd26e5a29ebdd65
Your Name <you@example.com>
2026-01-08 20:37:00 +0500

    Mehdi Abbas

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+import {ref} from 'vue'
+
+//Reactive Veriable
+
+const message = ref("hello bhaiyo")
+
+const ChangeWord = () =>
+{
+message.value = "Wah wah"
+}
+
+const BackWord =()=>
+{
+  message.value ="hello bhaiyo"
+}
+
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+   {{message}}
+  </p>
+  <button @click="ChangeWord" > next</button>
+<button @click="BackWord" > back</button>
+</template>
+
+<style scoped></style>
