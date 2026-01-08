fb42d7be74f608b1597754418df67a4f29e2ce3c
Your Name <you@example.com>
2026-01-09 00:47:04 +0500

    third

--- src/forloop.vue
+++ src/forloop.vue
@@ -2,17 +2,37 @@
 
 import {ref} from 'vue'
 
+const task = ref([])
+const newtask = ref('')
 
+const addtask = () => 
+{
+if(newtask.value)
+{
+    task.value.push(newtask.value)
+}
+newtask.value = ''
+}
 
 </script>
 
 <template>
- 
 
+<div>
 
+<input v-model = "newtask" placeholder="enter your name ">
+<button @click="addtask"> add </button>
 
-<p>connection</p>
+<ul>
 
+<li v-for="(task ,index) in task" key:index>
+{{task}}
+</li>
+
+</ul>
+</div>
 </template>
 
 <style scoped></style>
+
+
